739b50a5f471efe6dcc66a2e50b36cd339567159
MissawB <bahmani.wassim@outlook.fr>
2026-05-28 16:22:43 +0200

    security: remove hardcoded default credentials for Neo4j

--- src/pipeline/neo4j_client.py
+++ src/pipeline/neo4j_client.py
@@ -14,7 +14,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 NEO4J_URI = os.getenv("NEO4J_URI", "bolt://localhost:7687")
 NEO4J_USER = os.getenv("NEO4J_USER", "neo4j")
-NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "secretpassword")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
 
 ALLOWED_LABELS = {
     "Media", "Studio", "Person", "MicroTag", "Character", "CombatEvent", 
