ada72ff3a070bcf6518c2a3abdf3ccbb5a5d417d
MissawB <bahmani.wassim@outlook.fr>
2026-06-01 16:42:11 +0200

    fix(backend): resolve broken migration graph by pointing 0018_notification to 0016_alter_mediaitem_visual_embedding

--- backend/api/animetix/migrations/0018_notification.py
+++ backend/api/animetix/migrations/0018_notification.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('animetix', '0017_donation'),
+        ('animetix', '0016_alter_mediaitem_visual_embedding'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
