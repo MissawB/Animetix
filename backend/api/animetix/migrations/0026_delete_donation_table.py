bc9b47d75a1baf11080cbfdf5ba0bb1cb9554d54
MissawB <bahmani.wassim@outlook.fr>
2026-06-01 22:58:55 +0200

    db: add manual migration to drop animetix_donation table

---
+++ backend/api/animetix/migrations/0026_delete_donation_table.py
@@ -0,0 +1,17 @@
+# Generated manually on 2026-06-01
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('animetix', '0025_profile_personalization_settings'),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            sql="DROP TABLE IF EXISTS animetix_donation;",
+            reverse_sql=""
+        ),
+    ]
